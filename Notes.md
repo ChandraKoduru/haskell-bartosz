60896c4aff93154abbdef1a47f1656916ea7bc4b
Chandra Koduru <chandrakoduru@outlook.com>
2016-04-23 18:17:24 +0530

    first lecture Haskell-1-2

---
+++ Notes.md
@@ -0,0 +1,17 @@
+every function is an expression
+
+equational reasoning -> replacing symbol with symbol definition (just like a
+macro substitution)
+
+:q -- quit
+:l <file> -- load
+:r -- reload
+:i -- info
+:t <symbol>
+
+print :: Show a => a -> IO ()
+putStr :: String -> IO ()
+putStrLn :: String -> IO ()
+
+
+$ - > weak binding and right assicative
--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,24 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc
+{-
+main = print (sqDist 3 4)
+main = putStr (show (sqDist 3 4))
+main = putStrLn (show (sqDist 3 4))
+main = putStrLn "Hello World!"
+ -}
+
+-- | distance from the origin
+sqDist :: Num a => a -> a -> a
+-- sqDist :: Int -> Int -> Int
+sqDist x y = x^2 + y^2
+
+
+sqDist2 :: Num a => (a, a) -> a
+sqDist2 (x, y) = x^2 + y ^2 -- pattern match for pair
+
+sqDist3 :: Num a => (a, a) -> a
+sqDist3 p = (fst p)^2 + (snd p)^2
+
+-- main = print (sqDist3 (3,4))
+main = print $ sqDist3 $ (3,4) -- $ weak binding ..very weak, right associative
